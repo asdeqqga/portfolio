9798751e525412d153d50d2fe1c38e97a223756d
asdeqqga <111490458+asdeqqga@users.noreply.github.com>
2023-05-11 14:27:57 +0900

    Update EmailService.java

--- GCInside/src/main/java/kr/co/gcInside/service/EmailService.java
+++ GCInside/src/main/java/kr/co/gcInside/service/EmailService.java
@@ -32,16 +32,10 @@ public class EmailService {
         HtmlEmail email = new HtmlEmail();
         email.setHostName("smtp.gmail.com");
         email.setSmtpPort(587);
-<<<<<<< HEAD
         // username, password 로변경하면 더 깔끔함 .gitignore에 제외파일설정할 필요 X
         email.setAuthenticator(new DefaultAuthenticator(username, password));
         email.setSSLOnConnect(true);
         email.setFrom(username, "관리자"); //메일발송시 보내는이메일,이름
-=======
-        email.setAuthenticator(new DefaultAuthenticator(username, password));
-        email.setSSLOnConnect(true);
-        email.setFrom(username, "관리자");
->>>>>>> ab3a6614f79497f61b65a44e14346a2cd5edff9d
         email.addTo(toEmail);
         email.setSubject(subject);
         email.setCharset("utf-8"); 
